6400cfcdbcfd13abab7cb60f07a8bdc3fd8d35f2
shubham <katangleshubham@gmail.com>
2021-07-08 15:19:20 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import { Switch, Redirect } from 'react-router-dom'
